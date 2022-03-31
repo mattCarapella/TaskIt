64c9e8df600131d95c08f49106771d5bad3395e3
mattCarapella <mcarapel@buffalo.edu>
2022-03-31 18:44:52 -0400

    Add Image upload

--- TaskManager/Core/ViewModels/EditUserViewModel.cs
+++ TaskManager/Core/ViewModels/EditUserViewModel.cs
@@ -7,6 +7,8 @@ public class EditUserViewModel
 {
     public ApplicationUser User { get; set; }
 
+    public IFormFile? ProfilePicture { get; set; }
+
     public IList<SelectListItem> Roles { get; set; }
 
 }
