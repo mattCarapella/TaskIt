0c4055e433616320ab9200369cf043dca9d3d747
mattCarapella <mcarapel@buffalo.edu>
2022-05-22 18:03:46 -0400

    Add File model

---
+++ TaskManager/Models/FileModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models;
+
+public class FileModel
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    public byte[] Data { get; set; } = Array.Empty<byte>();
+
+    [Required, StringLength(50)]
+    public string FileType { get; set; }
+
+    [Required, StringLength(10)]
+    public string Extension { get; set; }
+
+    [Required, StringLength(300)]
+    public string Description { get; set; }
+
+    public string UploadedByUserId { get; set; } = String.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    
+}
